345a58aaccad46ae5ed54ad5cbe99eaf0454dead
Wyatt Arent <warent@Arentbook.Home>
2016-06-14 23:45:54 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var autoprefix  = require('gulp-autoprefixer');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['html', 'media', 'sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/html/*.html", ['html']);
+});
+
+gulp.task('html', function() {
+  return gulp.src("src/html/*.html")
+    .pipe(gulp.dest("app"));
+});
+
+gulp.task('media', function() {
+  return gulp.src("src/media/*")
+    .pipe(gulp.dest("app"))
+    .pipe(browserSync.stream());
+})
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/scss/*.scss")
+      .pipe(sass())
+      .pipe(autoprefix())
+      .pipe(gulp.dest("app"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
